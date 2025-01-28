6dafc843192d9696b06e62fe4a239c668b4a3a74
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2025-01-28 11:22:12 -0300

    Introduction to javascript (chapter 1)

---
+++ src/chapter-1/tour.js
@@ -0,0 +1,25 @@
+function main ()    {
+    let x;
+    x = 10;
+    console.log(x);
+    x = -10;
+    console.log(x);
+    x = 3.14;
+    console.log(x);
+    x = -3.14;
+    console.log(x);
+    x = true;
+    console.log(x);
+    x = false;
+    console.log(x);
+    x = 'a';
+    console.log(x);
+    x = "JavaScript";
+    console.log(x);
+    x = null;
+    console.log(x);
+    x = undefined;
+    console.log(x);
+}
+
+main();
