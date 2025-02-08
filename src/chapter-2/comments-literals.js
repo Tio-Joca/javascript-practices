b8fa21cc22e9733ce85a7e4bb50aa6d14f4057e5
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2025-02-08 13:18:04 -0300

    Creation of chapter-2 directory

---
+++ src/chapter-2/comments-literals.js
@@ -0,0 +1,29 @@
+// Single-line comment
+console.log("The line of text right above this statement in the source code is a single-line comment.\nEvery single-line comment starts with \"//\".");
+
+/*
+    Multi-line comment
+*/
+console.log("The line of text right above this statement in the source code is a multi-line comment.\nEvery multi-line comment starts with \"/*\" and ends with \"*/\".");
+
+console.log("As you can see, after you ran the code, the comments doesn't appear in the standard output.\nTo visualize the comments, you will need to view the source code.\n");
+
+// Literals
+console.log(12);
+console.log(-12);
+console.log(3.14);
+console.log(-3.14);
+console.log('A');
+console.log("a");
+console.log('Hello, I\'m Joaquim!');
+console.log("Hi, I\'m Joaquim!");
+console.log(true);
+console.log(false);
+console.log(null);
+console.log(undefined);
+
+/*
+    PS: As you can see, you can use single or double quotes pairs to contain a single character or text.
+    But, for convention to differentiate a single character from a text, use single quotes for a single
+    character and double quotes for text, even JavaScript considering both as strings.
+*/
