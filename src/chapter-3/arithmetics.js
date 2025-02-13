97d81c6dfc0d5c64ff37e0ce392887b5e82ab14f
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2025-02-13 19:59:11 -0300

    Add arithmetics.js to src/chapter-3 directory

---
+++ src/chapter-3/arithmetics.js
@@ -0,0 +1,73 @@
+// Arithmetics in JavaScript
+
+// Addition
+console.log(2 + 3);
+console.log(-2 + 3);
+console.log(2 + -3);
+console.log((-2 + -3) + '\n');
+
+// Subtraction
+console.log(512 - 128);
+console.log(-512 - 128);
+console.log(512 - -128);
+console.log((-512 - -128) + '\n');
+
+// Multiplication
+console.log(8 * 8);
+console.log(-8 * 8);
+console.log(8 * -8);
+console.log((-8 * -8) + '\n');
+
+// Division
+console.log(12.4 / 6.2);
+console.log(-12.4 / 6.2);
+console.log(12.4 / -6.2);
+console.log((-12.4 / -6.2) + '\n');
+
+// Exponentiation
+console.log(2 ** 9);
+console.log((-2) ** 9);
+console.log(2 ** -9);
+console.log(((-2) ** -9) + '\n');
+
+// Modulo (Remainder)
+console.log(5 % 3);
+console.log(-5 % 3);
+console.log(5 % -3);
+console.log((-5 % -3) + '\n');
+
+// Math methods
+console.log(Math.pow(2, 10));
+console.log(Math.round(2.5));
+console.log(Math.ceil(2.1));
+console.log(Math.floor(2.1));
+console.log(Math.abs(-5));
+console.log(Math.max(12, 3, 56, -234));
+console.log(Math.min(12, 3, 56, -234));
+console.log(Math.random());
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.sqrt(144));
+console.log(Math.pow(27, 1/3));
+console.log(Math.sin(45));
+console.log(Math.cos(45));
+console.log(Math.tan(45));
+console.log(Math.log(10));
+console.log(Math.log(100) / Math.LN10);
+console.log(Math.log(512) / Math.LN2);
+console.log((Math.exp(10)) + '\n');
+
+// ES6 additional math methods
+console.log(Math.cbrt(64));
+console.log(Math.hypot(3, 4));
+console.log(Math.log10(1000000));
+console.log(Math.log2(4096));
+console.log(Math.log1p(10));
+console.log(Math.expm1(10));
+console.log(Math.sign(23));
+console.log(Math.imul(1024 * 1024));
+console.log(Math.clz32(32));
+console.log(Math.trunc(23.3874));
+console.log(Math.fround(43.5));
+console.log(Math.asin(1));
+console.log((Math.asinh(12)) + '\n');
